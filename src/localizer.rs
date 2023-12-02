f6acd916defd4b1da372e3a50dca13e1c2d671f4
Skylar Gilfeather <sgilfeather@nasuni.com>
2023-12-02 14:52:04 -0500

    initial localizer work

--- src/localizer.rs
+++ src/localizer.rs
@@ -1,6 +1,48 @@
 use crate::hardware_data_manager::Update;
 use crate::Point;
 
-pub fn localize_points(_measurements: &[Update]) -> Vec<Point> {
-    todo!() // ISSUE 34
+/** localize_points()
+ * @brief   Given a list of `Update` structs, computes the positions of the blocks
+ * @param   Array of Updates, which describe a block's rough azimuth relative to another
+ * @returns A vector of Points
+ */
+pub fn localize_points(measurements: &[Update]) -> Vec<Point> {
+    // Debug printing code!!
+    // TODO: remove
+    fn print_update(m: Update) {
+        eprintln!("src={:?}, dst={:?}, elv={:?}, azm={:?}", m.src, m.dst, m.elv, m.azm);
+    }
+    eprintln!("ALL UPDATES");
+    measurements.iter().for_each(print_update);
+    // end debug code
+
+    // For now, assume constant range
+    let range = 1;
+
+    // For now, generate points just based on angles FROM listener
+    // no duplicate updates for the same src, dst pair
+    // NEEDSWORK: not averaging measurements over multiple updates?
+    measurements.iter().filter(|m| m.src == 0).map(|m| {
+        print_update(m); // TODO: remove
+        // working in the 2D plan, elv is 0 for now
+        let x = range * m.azm.cos() * m.elv.sin();
+        let y = range * m.azm.sin() * m.elv.sin();
+        Point(x, y)
+    }).collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let updates: [Update] = [
+            Update(0, 1, 0, 0),   // block 1 is straight ahead of the listener
+            Update(0, 2, 0, 1.57) // block 2 is to the right of the listener
+        ];
+        let points = localize_points(&updates);
+        eprintln!("{:?}", points);
+    }
 }
