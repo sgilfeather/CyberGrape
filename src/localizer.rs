191033bbda587091fe18d15b795e984b59ddf4c3
Tyler Thompson <tyler.thompson@tufts.edu>
2023-12-02 14:59:55 -0500

    hardware localization debug

--- src/localizer.rs
+++ src/localizer.rs
@@ -9,28 +9,34 @@ use crate::Point;
 pub fn localize_points(measurements: &[Update]) -> Vec<Point> {
     // Debug printing code!!
     // TODO: remove
-    fn print_update(m: Update) {
-        eprintln!("src={:?}, dst={:?}, elv={:?}, azm={:?}", m.src, m.dst, m.elv, m.azm);
+    fn print_update(m: &Update) {
+        eprintln!(
+            "src={:?}, dst={:?}, elv={:?}, azm={:?}",
+            m.src, m.dst, m.elv, m.azm
+        );
     }
     eprintln!("ALL UPDATES");
-    measurements.iter().for_each(print_update);
+    measurements.iter().for_each(|u| print_update(&u));
     // end debug code
 
     // For now, assume constant range
-    let range = 1;
+    let range = 1.0;
 
     // For now, generate points just based on angles FROM listener
     // no duplicate updates for the same src, dst pair
     // NEEDSWORK: not averaging measurements over multiple updates?
-    measurements.iter().filter(|m| m.src == 0).map(|m| {
-        print_update(m); // TODO: remove
-        // working in the 2D plan, elv is 0 for now
-        let x = range * m.azm.cos() * m.elv.sin();
-        let y = range * m.azm.sin() * m.elv.sin();
-        Point(x, y)
-    }).collect()
+    measurements
+        .iter()
+        .filter(|m| m.src == 0)
+        .map(|m| {
+            print_update(m); // TODO: remove
+                             // working in the 2D plan, elv is 0 for now
+            let x = range * m.azm.cos() * m.elv.sin();
+            let y = range * m.azm.sin() * m.elv.sin();
+            Point { x, y }
+        })
+        .collect()
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -38,9 +44,19 @@ mod tests {
 
     #[test]
     fn test1() {
-        let updates: [Update] = [
-            Update(0, 1, 0, 0),   // block 1 is straight ahead of the listener
-            Update(0, 2, 0, 1.57) // block 2 is to the right of the listener
+        let updates = [
+            Update {
+                src: 0,
+                dst: 1,
+                elv: 0.0,
+                azm: 0.0,
+            }, // block 1 is straight ahead of the listener
+            Update {
+                src: 0,
+                dst: 2,
+                elv: 0.0,
+                azm: 1.57,
+            }, // block 2 is to the right of the listener
         ];
         let points = localize_points(&updates);
         eprintln!("{:?}", points);
