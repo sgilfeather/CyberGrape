44807688de08bdbac138b288be0d0c22ee538ec4
liam-strand <Liam.Strand@tufts.edu>
2024-01-15 22:17:28 -0500

    first attempt at binauralization API

--- src/saf.rs
+++ src/saf.rs
@@ -1,39 +1,42 @@
 //! A safe api into the Spatial Audio Framework.
 
+use std::ops::Index;
+
 use crate::saf_raw;
 
 use libc::c_void;
 
-/// TODO
-pub fn bn_create() -> *mut c_void {
-    unsafe {
+pub trait Binauraliser {
+    fn process<B>(&self, buffers: &[(Position, B)]) -> Vec<B>
+    where
+        B: Index<usize, Output = f32>;
+}
+
+pub struct Position {}
+
+pub struct BinauraliserNF {
+    h_bin: *mut c_void,
+}
+
+impl BinauraliserNF {
+    pub fn new() -> Self {
         let mut h_bin = std::ptr::null_mut();
-        saf_raw::binauraliserNF_create(std::ptr::addr_of_mut!(h_bin));
-        h_bin
+        unsafe {
+            saf_raw::binauraliserNF_create(std::ptr::addr_of_mut!(h_bin));
+        }
+        BinauraliserNF { h_bin }
     }
 }
 
-/// TODO
-pub fn bn_destroy(h_bin: &mut *mut c_void) {
-    unsafe {
-        saf_raw::binauraliserNF_destroy(h_bin);
+impl Drop for BinauraliserNF {
+    fn drop(&mut self) {
+        unsafe {
+            saf_raw::binauraliserNF_destroy(std::ptr::addr_of_mut!(self.h_bin));
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn ph_bin_is_created() {
-        let res = bn_create();
-        assert!(!res.is_null());
-    }
-
-    #[test]
-    fn ph_bin_is_destroyed() {
-        let mut res = bn_create();
-        bn_destroy(&mut res);
-        assert!(res.is_null());
-    }
 }
