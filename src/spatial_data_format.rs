7497047cd8f91c642bd02f078d599928c6d7f945
liam-strand <Liam.Strand@tufts.edu>
2024-02-13 13:01:26 -0500

    add methods to read data out of a GrapeFile object

--- src/spatial_data_format.rs
+++ src/spatial_data_format.rs
@@ -2,15 +2,12 @@
 //!
 //!
 
-#![allow(unused)]
 use serde::{Deserialize, Serialize};
 use std::{
     fs::File,
     io::{Read, Write},
     path::Path,
 };
-
-const A_FLOAT: f32 = 12.07843112945556640625;
 
 #[derive(Debug, Clone, PartialEq)]
 struct GrapeFile {
@@ -54,35 +51,29 @@ impl GrapeFile {
     }
 
     pub fn to_file(&self, path: impl AsRef<Path>) -> Result<(), GrapeFileError> {
-        let mut handle = File::create(path).map_err(|e| GrapeFileError::IoError(e))?;
+        let mut handle = File::create(path).map_err(GrapeFileError::IoError)?;
 
-        let h_str = ron::ser::to_string(&self.header).map_err(|e| GrapeFileError::RonError(e))?;
+        let h_str = ron::ser::to_string(&self.header).map_err(GrapeFileError::RonError)?;
         let h_buf = h_str.as_bytes();
 
-        handle
-            .write_all(h_buf)
-            .map_err(|e| GrapeFileError::IoError(e))?;
+        handle.write_all(h_buf).map_err(GrapeFileError::IoError)?;
 
-        handle
-            .write_all(&[0xFF])
-            .map_err(|e| GrapeFileError::IoError(e))?;
+        handle.write_all(&[0xFF]).map_err(GrapeFileError::IoError)?;
 
         let s_buf: Vec<u8> = self.samples.iter().flat_map(|f| f.to_be_bytes()).collect();
 
-        handle
-            .write_all(&s_buf)
-            .map_err(|e| GrapeFileError::IoError(e))?;
+        handle.write_all(&s_buf).map_err(GrapeFileError::IoError)?;
 
         Ok(())
     }
 
     pub fn from_file(path: impl AsRef<Path>) -> Result<Self, GrapeFileError> {
-        let mut handle = File::open(path).map_err(|e| GrapeFileError::IoError(e))?;
+        let mut handle = File::open(path).map_err(GrapeFileError::IoError)?;
 
         let mut raw_text = Vec::new();
         handle
             .read_to_end(&mut raw_text)
-            .map_err(|e| GrapeFileError::IoError(e))?;
+            .map_err(GrapeFileError::IoError)?;
 
         let delim_idx = raw_text
             .iter()
@@ -95,7 +86,7 @@ impl GrapeFile {
             .ok_or(GrapeFileError::NoDelimiter)?;
 
         let header = ron::de::from_bytes::<GrapeFileHeader>(header_buf)
-            .map_err(|e| GrapeFileError::RonSpannedError(e))?;
+            .map_err(GrapeFileError::RonSpannedError)?;
 
         let samples: Vec<f32> = samples_buf
             .chunks(4)
@@ -107,6 +98,75 @@ impl GrapeFile {
             .collect::<Result<Vec<f32>, GrapeFileError>>()?;
 
         Ok(GrapeFile { header, samples })
+    }
+
+    pub fn streams_native_sample_rate(&self) -> (u64, Vec<(GrapeTag, Vec<f32>)>) {
+        let sample_vecs = self.get_raw_streams();
+
+        let res_vecs = Self::attach_tags(&self.header.tags, sample_vecs);
+
+        (self.header.sample_rate, res_vecs)
+    }
+
+    pub fn streams_with_sample_rate(&self, sample_rate: u64) -> Vec<(GrapeTag, Vec<f32>)> {
+        if sample_rate == self.header.sample_rate {
+            self.streams_native_sample_rate().1
+        } else if sample_rate > self.header.sample_rate {
+            self.streams_interpolated(sample_rate)
+        } else {
+            self.streams_quantized(sample_rate)
+        }
+    }
+
+    fn attach_tags(tags: &[GrapeTag], samples: Vec<Vec<f32>>) -> Vec<(GrapeTag, Vec<f32>)> {
+        assert_eq!(tags.len(), samples.len());
+        tags.iter().cloned().zip(samples.into_iter()).collect()
+    }
+
+    fn get_raw_streams(&self) -> Vec<Vec<f32>> {
+        let n_streams = self.header.n_streams as usize;
+        let stream_len = self.samples.len() / n_streams;
+        let mut sample_vecs = vec![Vec::with_capacity(stream_len); n_streams];
+
+        for (i, &e) in self.samples.iter().enumerate() {
+            let stream_idx = i % n_streams;
+            sample_vecs[stream_idx].push(e);
+        }
+        sample_vecs
+    }
+
+    fn streams_interpolated(&self, sample_rate: u64) -> Vec<(GrapeTag, Vec<f32>)> {
+        debug_assert!(sample_rate > self.header.sample_rate);
+        let samples_per_pt = sample_rate as usize / self.header.sample_rate as usize;
+        let raw_streams = self.get_raw_streams();
+        let interpolated_streams = raw_streams
+            .into_iter()
+            .map(|v| {
+                v.windows(2)
+                    .flat_map(|w| {
+                        let step = (w[1] - w[0]) / samples_per_pt as f32;
+                        (0..samples_per_pt)
+                            .map(|i| w[0] + i as f32 * step)
+                            .collect::<Vec<_>>()
+                    }).collect()
+            })
+            .collect();
+        Self::attach_tags(&self.header.tags, interpolated_streams)
+    }
+
+    fn streams_quantized(&self, sample_rate: u64) -> Vec<(GrapeTag, Vec<f32>)> {
+        debug_assert!(sample_rate < self.header.sample_rate);
+        let pts_per_sample = self.header.sample_rate as usize / sample_rate as usize;
+        let raw_streams = self.get_raw_streams();
+        let quantized_streams = raw_streams
+            .into_iter()
+            .map(|v| {
+                v.chunks(pts_per_sample)
+                    .map(|c| c.iter().sum::<f32>() / c.len() as f32)
+                    .collect()
+            })
+            .collect();
+        Self::attach_tags(&self.header.tags, quantized_streams)
     }
 }
 
@@ -157,7 +217,7 @@ impl GrapeFileBuilder {
 
         let mut samples = Vec::new();
 
-        if sample_vecs.len() != 0 {
+        if !sample_vecs.is_empty() {
             let shortest = sample_vecs.iter().map(|v| v.len()).min().unwrap();
             samples.reserve_exact(shortest * self.n_streams as usize);
             for sample_idx in 0..shortest {
@@ -188,7 +248,7 @@ impl GrapeFileBuilder {
 
         let mut samples = Vec::new();
 
-        if sample_vecs.len() != 0 {
+        if !sample_vecs.is_empty() {
             let longest = sample_vecs.iter().map(|v| v.len()).max().unwrap();
             let lasts: Vec<f32> = sample_vecs
                 .iter()
@@ -199,10 +259,9 @@ impl GrapeFileBuilder {
             for sample_idx in 0..longest {
                 for stream_idx in 0..self.n_streams as usize {
                     samples.push(
-                        sample_vecs[stream_idx as usize]
+                        *sample_vecs[stream_idx]
                             .get(sample_idx)
-                            .unwrap_or(&lasts[stream_idx])
-                            .clone(),
+                            .unwrap_or(&lasts[stream_idx]),
                     );
                 }
             }
@@ -230,12 +289,13 @@ impl GrapeFileBuilder {
 }
 
 #[cfg(test)]
+const A_FLOAT: f32 = 12.07843112945556640625;
 mod tests {
     use super::*;
 
     #[test]
     fn write_and_read() {
-        let mut tempfile = tempfile::NamedTempFile::new().unwrap();
+        let tempfile = tempfile::NamedTempFile::new().unwrap();
         let path = tempfile.path();
         let data = GrapeFile::builder()
             .set_samplerate(1000)
@@ -247,5 +307,89 @@ mod tests {
         data.to_file(path).unwrap();
         let read_data = GrapeFile::from_file(path).unwrap();
         assert_eq!(data, read_data);
+    }
+
+    #[test]
+    fn native_sample_rate_read() {
+        let stream_data = vec![
+            (GrapeTag::X, vec![A_FLOAT; 4]),
+            (GrapeTag::Y, vec![A_FLOAT; 4]),
+        ];
+
+        let data = GrapeFile::builder()
+            .set_samplerate(1000)
+            .add_stream(&stream_data[0].1, stream_data[0].0)
+            .add_stream(&stream_data[1].1, stream_data[1].0)
+            .build()
+            .unwrap();
+
+        let (sr, streams) = data.streams_native_sample_rate();
+        assert_eq!(1000, sr);
+        assert_eq!(stream_data, streams);
+    }
+
+    #[test]
+    fn quantize_read() {
+        let stream_data = vec![
+            (GrapeTag::X, vec![A_FLOAT; 4]),
+            (GrapeTag::Y, vec![A_FLOAT; 4]),
+        ];
+
+        let data = GrapeFile::builder()
+            .set_samplerate(1000)
+            .add_stream(&stream_data[0].1, stream_data[0].0)
+            .add_stream(&stream_data[1].1, stream_data[1].0)
+            .build()
+            .unwrap();
+
+        let streams = data.streams_with_sample_rate(500);
+        assert_eq!(
+            vec![
+                (GrapeTag::X, vec![A_FLOAT; 2]),
+                (GrapeTag::Y, vec![A_FLOAT; 2]),
+            ],
+            streams
+        );
+    }
+
+    #[test]
+    fn interpolate_same() {
+        let stream_data = vec![
+            (GrapeTag::X, vec![A_FLOAT; 4]),
+            (GrapeTag::Y, vec![A_FLOAT; 4]),
+        ];
+
+        let data = GrapeFile::builder()
+            .set_samplerate(1000)
+            .add_stream(&stream_data[0].1, stream_data[0].0)
+            .add_stream(&stream_data[1].1, stream_data[1].0)
+            .build()
+            .unwrap();
+
+        let streams = data.streams_with_sample_rate(2000);
+        assert_eq!(
+            vec![
+                (GrapeTag::X, vec![A_FLOAT; 6]),
+                (GrapeTag::Y, vec![A_FLOAT; 6]),
+            ],
+            streams
+        );
+    }
+
+    #[test]
+    fn interpolate_range() {
+        let stream_data = vec![0.0, 0.2, 0.8];
+
+        let data = GrapeFile::builder()
+            .set_samplerate(5)
+            .add_stream(&stream_data, GrapeTag::Yaw)
+            .build()
+            .unwrap();
+
+        let streams = data.streams_with_sample_rate(10);
+        assert_eq!(
+            vec![(GrapeTag::Yaw, vec![0.0, 0.1, 0.2, 0.5])],
+            streams
+        );
     }
 }
