ad70aff035bdd1d7b274a1bd4365bcfc7406c448
Tyler Thompson <tyler.thompson@tufts.edu>
2024-04-22 17:29:41 -0400

    Make it work

--- src/time_domain_buffer.rs
+++ src/time_domain_buffer.rs
@@ -1,3 +1,5 @@
+use crate::saf::BufferMetadata;
+
 pub struct TDBufMeta {
     data: Vec<Vec<BufferMetadata>>,
     num_tags: usize,
@@ -16,7 +18,37 @@ impl TDBufMeta {
         self.data.push(data.to_vec());
     }
 
-    fn dump(&self) -> Vec<Vec<BufferMetadata>> {
+    fn dump(self) -> Vec<Vec<BufferMetadata>> {
         self.data
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_buf_init() {
+        let buf = TDBufMeta::new(2);
+        assert_eq!(buf.num_tags, 2);
+        let update = [
+            BufferMetadata {
+                azimuth: 90.0,
+                elevation: 0.0,
+                range: 1.0,
+                gain: 1.0,
+            },
+            BufferMetadata {
+                azimuth: 45.0,
+                elevation: 45.0,
+                range: 1.0,
+                gain: 1.0,
+            },
+        ];
+        let mut buf = TDBufMeta::new(2);
+        buf.add(&update);
+        let data = buf.dump();
+        assert_eq!(data.len(), 1);
+        assert_eq!(data[0].len(), 2);
     }
 }
